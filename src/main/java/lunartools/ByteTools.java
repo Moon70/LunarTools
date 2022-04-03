0898b859c142ec76e7be15ec01c5f2b3cb572381
Moon70 <thomas.mattel@drei.at>
2022-04-03 17:11:25 +0200

    added some ByteTool methods for 'little endian'

--- src/main/java/lunartools/ByteTools.java
+++ src/main/java/lunartools/ByteTools.java
@@ -15,7 +15,21 @@ public class ByteTools {
 		int byte3=longword&0xff;
 		return new byte[] {(byte)byte0,(byte)byte1,(byte)byte2,(byte)byte3};
 	}
-	
+
+	/**
+	 * Converts a longword, given as int value, to a little-endian bytearray.
+	 * 
+	 * @param longword
+	 * @return little endian bytearray
+	 */
+	public static byte[] lLongwordToBytearray(int longword) {
+		int byte0=(longword>>24)&0xff;
+		int byte1=(longword>>16)&0xff;
+		int byte2=(longword>>8)&0xff;
+		int byte3=longword&0xff;
+		return new byte[] {(byte)byte3,(byte)byte2,(byte)byte1,(byte)byte0};
+	}
+
 	/**
 	 * Converts a longword, given as long value, to a big-endian bytearray.
 	 * 
@@ -29,7 +43,21 @@ public class ByteTools {
 		long byte3=longword&0xff;
 		return new byte[] {(byte)byte0,(byte)byte1,(byte)byte2,(byte)byte3};
 	}
-	
+
+	/**
+	 * Converts a longword, given as long value, to a little-endian bytearray.
+	 * 
+	 * @param longword
+	 * @return little endian bytearray
+	 */
+	public static byte[] lLongwordToBytearray(long longword) {
+		long byte0=(longword>>24)&0xff;
+		long byte1=(longword>>16)&0xff;
+		long byte2=(longword>>8)&0xff;
+		long byte3=longword&0xff;
+		return new byte[] {(byte)byte3,(byte)byte2,(byte)byte1,(byte)byte0};
+	}
+
 	/**
 	 * Converts a word, given as int value, to a big-endian bytearray.
 	 * 
@@ -41,7 +69,19 @@ public class ByteTools {
 		int byte1=word&0xff;
 		return new byte[] {(byte)byte0,(byte)byte1};
 	}
-	
+
+	/**
+	 * Converts a word, given as int value, to a little-endian bytearray.
+	 * 
+	 * @param word
+	 * @return little endian bytearray
+	 */
+	public static byte[] lWordToBytearray(int word) {
+		int byte0=(word>>8)&0xff;
+		int byte1=word&0xff;
+		return new byte[] {(byte)byte1,(byte)byte0};
+	}
+
 	/**
 	 * Converts 4 bytes, beginning from <code>offset</code> of the given bytearray to
 	 * a longword.
@@ -60,6 +100,59 @@ public class ByteTools {
 	}
 
 	/**
+	 * Converts 4 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a signed longword.
+	 * <br>The byte order is big endian.
+	 * 
+	 * @param bytearray The bytearray containing a longword
+	 * @param offset The offset to the to the longword
+	 * @return The signed longword created from a bytearry in big endian byteorder
+	 */
+	public static long bBytearrayToSignedLongword(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		int byte2=bytearray[offset+2]&0xff;
+		int byte3=bytearray[offset+3]&0xff;
+		long result=((long)byte0<<24)|(byte1<<16)|(byte2<<8)|byte3;
+		return (result & 0b10000000000000000000000000000000)==0?result:result-0x100000000l;
+	}
+
+	/**
+	 * Converts 4 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a longword.
+	 * <br>The byte order is little endian.
+	 * 
+	 * @param bytearray The bytearray containing a longword
+	 * @param offset The offset to the to the longword
+	 * @return The longword created from a bytearry in little endian byteorder
+	 */
+	public static long lBytearrayToLongword(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		int byte2=bytearray[offset+2]&0xff;
+		int byte3=bytearray[offset+3]&0xff;
+		return ((long)byte3<<24)|(byte2<<16)|(byte1<<8)|byte0;
+	}
+
+	/**
+	 * Converts 4 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a signed longword.
+	 * <br>The byte order is little endian.
+	 * 
+	 * @param bytearray The bytearray containing a longword
+	 * @param offset The offset to the to the longword
+	 * @return The signed longword created from a bytearry in little endian byteorder
+	 */
+	public static long lBytearrayToSignedLongword(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		int byte2=bytearray[offset+2]&0xff;
+		int byte3=bytearray[offset+3]&0xff;
+		long result=((long)byte3<<24)|(byte2<<16)|(byte1<<8)|byte0;
+		return (result & 0b10000000000000000000000000000000)==0?result:result-0x100000000l;
+	}
+
+	/**
 	 * Converts 2 bytes, beginning from <code>offset</code> of the given bytearray to
 	 * a word.
 	 * <br>The byte order is big endian.
@@ -75,6 +168,53 @@ public class ByteTools {
 	}
 
 	/**
+	 * Converts 2 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a signed word.
+	 * <br>The byte order is big endian.
+	 * 
+	 * @param bytearray The bytearray containing a word
+	 * @param offset The offset to the to the word
+	 * @return The signed word created from a bytearry in big endian byteorder
+	 */
+	public static int bBytearrayToSignedWord(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		int result=(byte0<<8)|byte1;
+		return (result & 0b1000000000000000)==0?result:result-0x10000;
+	}
+
+	/**
+	 * Converts 2 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a word.
+	 * <br>The byte order is little endian.
+	 * 
+	 * @param bytearray The bytearray containing a word
+	 * @param offset The offset to the to the word
+	 * @return The word created from a bytearry in little endian byteorder
+	 */
+	public static int lBytearrayToWord(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		return (byte1<<8)|byte0;
+	}
+
+	/**
+	 * Converts 2 bytes, beginning from <code>offset</code> of the given bytearray to
+	 * a signed word.
+	 * <br>The byte order is little endian.
+	 * 
+	 * @param bytearray The bytearray containing a word
+	 * @param offset The offset to the to the word
+	 * @return The signed word created from a bytearry in little endian byteorder
+	 */
+	public static int lBytearrayToSignedWord(byte[] bytearray, int offset) {
+		int byte0=bytearray[offset]&0xff;
+		int byte1=bytearray[offset+1]&0xff;
+		int result=(byte1<<8)|byte0;
+		return (result & 0b1000000000000000)==0?result:result-0x10000;
+	}
+
+	/**
 	 * Writes a longword to a bytearray at the given offset, overwriting the bytes.
 	 * <br>The byte order is big endian.
 	 * 
@@ -84,6 +224,21 @@ public class ByteTools {
 	 */
 	public static void bWriteLongwordToBytearray(byte[] bytes,int offset,long longword) {
 		byte[] ba=bLongwordToBytearray(longword);
+		for(int i=0;i<4;i++) {
+			bytes[offset+i]=ba[i];
+		}
+	}
+
+	/**
+	 * Writes a longword to a bytearray at the given offset, overwriting the bytes.
+	 * <br>The byte order is little endian.
+	 * 
+	 * @param bytes The bytearray to write the longword
+	 * @param offset offset to the bytearray position to write the longword
+	 * @param longword The longword
+	 */
+	public static void lWriteLongwordToBytearray(byte[] bytes,int offset,long longword) {
+		byte[] ba=lLongwordToBytearray(longword);
 		for(int i=0;i<4;i++) {
 			bytes[offset+i]=ba[i];
 		}
@@ -115,7 +270,7 @@ public class ByteTools {
 		}
 		return ba;
 	}
-	
+
 	/**
 	 * Checks if one bytearray is equal another bytearray, or part of another bytearray
 	 * at a given offset.
--- src/test/java/lunartools/ByteToolsTest.java
+++ src/test/java/lunartools/ByteToolsTest.java
@@ -14,10 +14,24 @@ class ByteToolsTest {
 	}
 
 	@Test
+	void positiveIntLongwordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0x90,(byte)0x80,0x02,0x01};
+		int longword=0x01028090;
+		assertArrayEquals(expected,ByteTools.lLongwordToBytearray(longword));
+	}
+
+	@Test
 	void negativeIntLongwordIsConvertedToBigEndianBytearrayCorrectly() {
-		byte[] expected=new byte[] {(byte)0xff,(byte)0xff,(byte)0xff,(byte)0xff};
-		int longword=0xffffffff;
+		byte[] expected=new byte[] {(byte)0xff,(byte)0xfc,(byte)0xfb,(byte)0xfa};
+		int longword=0xfffcfbfa;
 		assertArrayEquals(expected,ByteTools.bLongwordToBytearray(longword));
+	}
+
+	@Test
+	void negativeIntLongwordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0xfa,(byte)0xfb,(byte)0xfc,(byte)0xff};
+		int longword=0xfffcfbfa;
+		assertArrayEquals(expected,ByteTools.lLongwordToBytearray(longword));
 	}
 
 	@Test
@@ -28,10 +42,24 @@ class ByteToolsTest {
 	}
 
 	@Test
+	void positiveLongLongwordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0x90,(byte)0x80,0x02,0x01};
+		long longword=0x01028090;
+		assertArrayEquals(expected,ByteTools.lLongwordToBytearray(longword));
+	}
+
+	@Test
 	void negativeLongLongwordIsConvertedToBigEndianBytearrayCorrectly() {
 		byte[] expected=new byte[] {(byte)0xff,(byte)0xff,(byte)0xff,(byte)0xff};
 		long longword=0xffffffff;
 		assertArrayEquals(expected,ByteTools.bLongwordToBytearray(longword));
+	}
+
+	@Test
+	void negativeLongLongwordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0xfa,(byte)0xfb,(byte)0xfc,(byte)0xff};
+		int longword=0xfffcfbfa;
+		assertArrayEquals(expected,ByteTools.lLongwordToBytearray(longword));
 	}
 
 	@Test
@@ -42,6 +70,13 @@ class ByteToolsTest {
 	}
 
 	@Test
+	void positiveIntWordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0x80,0x02};
+		int word=0x0280;
+		assertArrayEquals(expected,ByteTools.lWordToBytearray(word));
+	}
+
+	@Test
 	void negativeIntWordIsConvertedToBigEndianBytearrayCorrectly() {
 		byte[] expected=new byte[] {(byte)0xff,(byte)0xff};
 		int word=0xffff;
@@ -49,13 +84,44 @@ class ByteToolsTest {
 	}
 
 	@Test
-	void potitiveLongValueIsExtractedFromBigEndianBytearrayCorrectly() {
+	void negativeIntWordIsConvertedToLittleEndianBytearrayCorrectly() {
+		byte[] expected=new byte[] {(byte)0xfa,(byte)0xff};
+		int word=0xfffa;
+		assertArrayEquals(expected,ByteTools.lWordToBytearray(word));
+	}
+
+	@Test
+	void positiveLongValueIsExtractedFromBigEndianBytearrayCorrectly() {
 		long expected=0x01028090;
 		byte[] ba=new byte[] {0x11,0x11,0x01,0x02,(byte)0x80,(byte)0x90,0x11,0x11};
 		int offsetToWantedBytearray=2;
 		assertEquals(expected,ByteTools.bBytearrayToLongword(ba,offsetToWantedBytearray));
 	}
-	
+
+	@Test
+	void positiveSignedLongValueIsExtractedFromBigEndianBytearrayCorrectly() {
+		long expected=0x01028090;
+		byte[] ba=new byte[] {0x11,0x11,0x01,0x02,(byte)0x80,(byte)0x90,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.bBytearrayToSignedLongword(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void positiveLongValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		long expected=0x01028090;
+		byte[] ba=new byte[] {0x11,0x11,(byte)0x90,(byte)0x80,0x02,0x01,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.lBytearrayToLongword(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void positiveSignedLongValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		long expected=0x01028090;
+		byte[] ba=new byte[] {0x11,0x11,(byte)0x90,(byte)0x80,0x02,0x01,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.lBytearrayToSignedLongword(ba,offsetToWantedBytearray));
+	}
+
 	@Test
 	void negativeLongValueIsExtractedFromBigEndianBytearrayCorrectly() {
 		long expected=0xff0280ffl;
@@ -63,23 +129,113 @@ class ByteToolsTest {
 		int offsetToWantedBytearray=1;
 		assertEquals(expected,ByteTools.bBytearrayToLongword(ba,offsetToWantedBytearray));
 	}
-	
+
 	@Test
-	void potitiveWordValueIsExtractedFromBigEndianBytearrayCorrectly() {
-		long expected=0x0180;
+	void negativeSignedLongValueIsExtractedFromBigEndianBytearrayCorrectly() {
+		long expected=-16909061l;
+		byte[] ba=new byte[] {0x11,(byte)0xfe,(byte)0xfd,(byte)0xfc,(byte)0xfb,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.bBytearrayToSignedLongword(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void negativeLongValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		long expected=0xff0280ffl;
+		byte[] ba=new byte[] {0x11,(byte)0xff,(byte)0x80,0x02,(byte)0xff,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.lBytearrayToLongword(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void negativeSignedLongValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		long expected=-16909061l;
+		byte[] ba=new byte[] {0x11,(byte)0xfb,(byte)0xfc,(byte)0xfd,(byte)0xfe,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.lBytearrayToSignedLongword(ba,offsetToWantedBytearray));
+	}
+
+
+	@Test
+	void positiveWordValueIsExtractedFromBigEndianBytearrayCorrectly() {
+		int expected=0x0180;
 		byte[] ba=new byte[] {0x11,0x11,0x01,(byte)0x80,0x11,0x11};
 		int offsetToWantedBytearray=2;
 		assertEquals(expected,ByteTools.bBytearrayToWord(ba,offsetToWantedBytearray));
 	}
-	
+
+	@Test
+	void positiveSignedWordValueIsExtractedFromBigEndianBytearrayCorrectly() {
+		int expected=0x0180;
+		byte[] ba=new byte[] {0x11,0x11,0x01,(byte)0x80,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.bBytearrayToSignedWord(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void positiveWordValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		int expected=0x0180;
+		byte[] ba=new byte[] {0x11,0x11,(byte)0x80,0x01,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.lBytearrayToWord(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void positiveSignedWordValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		int expected=0x0180;
+		byte[] ba=new byte[] {0x11,0x11,(byte)0x80,0x01,0x11,0x11};
+		int offsetToWantedBytearray=2;
+		assertEquals(expected,ByteTools.lBytearrayToSignedWord(ba,offsetToWantedBytearray));
+	}
+
 	@Test
 	void negativeWordValueIsExtractedFromBigEndianBytearrayCorrectly() {
-		long expected=0xff02l;
+		int expected=0xff02;
 		byte[] ba=new byte[] {0x11,(byte)0xff,0x02,0x11,0x11};
 		int offsetToWantedBytearray=1;
 		assertEquals(expected,ByteTools.bBytearrayToWord(ba,offsetToWantedBytearray));
 	}
-	
+
+	@Test
+	void negativeSignedWordValueIsExtractedFromBigEndianBytearrayCorrectly() {
+		int expected=-2;
+		byte[] ba=new byte[] {0x11,(byte)0xff,(byte)0xfe,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.bBytearrayToSignedWord(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void negativeWordValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		int expected=0xff02;
+		byte[] ba=new byte[] {0x11,0x02,(byte)0xff,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.lBytearrayToWord(ba,offsetToWantedBytearray));
+	}
+
+	@Test
+	void negativeSignedWordValueIsExtractedFromLittleEndianBytearrayCorrectly() {
+		int expected=-2;
+		byte[] ba=new byte[] {0x11,(byte)0xfe,(byte)0xff,0x11,0x11};
+		int offsetToWantedBytearray=1;
+		assertEquals(expected,ByteTools.lBytearrayToSignedWord(ba,offsetToWantedBytearray));
+	}
+
+
+	@Test
+	void positiveByteIsReadFromBytearrayCorrectly() {
+		int expected=0x14;
+		byte[] bytearray=new byte[]	{0x01,0x02,0x14,0x04,0x05};
+		int offsetToByte=2;
+		assertEquals(expected,ByteTools.bytearrayToByte(bytearray, offsetToByte));
+	}
+
+	@Test
+	void negativeByteIsReadFromBytearrayCorrectly() {
+		int expected=0xff;
+		byte[] bytearray=new byte[] {0x01,0x02,(byte)0xff,0x04,0x05};
+		int offsetToByte=2;
+		assertEquals(expected,ByteTools.bytearrayToByte(bytearray, offsetToByte));
+	}
+
 	@Test
 	void positiveLongwordIsWrittenToBigEndianBytearrayCorrectly() {
 		byte[] bytearray=new byte[]	{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
@@ -89,7 +245,17 @@ class ByteToolsTest {
 		ByteTools.bWriteLongwordToBytearray(bytearray,offsetToWriteLongword,longword);
 		assertArrayEquals(expected,bytearray);
 	}
-	
+
+	@Test
+	void positiveLongwordIsWrittenToLittleEndianBytearrayCorrectly() {
+		byte[] bytearray=new byte[]	{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+		byte[] expected=new byte[]	{0x01,0x02,0x14,0x13,0x12,0x11,0x07,0x08};
+		int longword=0x11121314;
+		int offsetToWriteLongword=2;
+		ByteTools.lWriteLongwordToBytearray(bytearray,offsetToWriteLongword,longword);
+		assertArrayEquals(expected,bytearray);
+	}
+
 	@Test
 	void negativeLongwordIsWrittenToBigEndianBytearrayCorrectly() {
 		byte[] bytearray=new byte[]	{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
@@ -101,19 +267,13 @@ class ByteToolsTest {
 	}
 
 	@Test
-	void positiveByetIsReadFromBytearrayCorrectly() {
-		int expected=0x14;
-		byte[] bytearray=new byte[]	{0x01,0x02,0x14,0x04,0x05};
-		int offsetToByte=2;
-		assertEquals(expected,ByteTools.bytearrayToByte(bytearray, offsetToByte));
-	}
-
-	@Test
-	void negativeByetIsReadFromBytearrayCorrectly() {
-		int expected=0xff;
-		byte[] bytearray=new byte[] {0x01,0x02,(byte)0xff,0x04,0x05};
-		int offsetToByte=2;
-		assertEquals(expected,ByteTools.bytearrayToByte(bytearray, offsetToByte));
+	void negativeLongwordIsWrittenToLittleEndianBytearrayCorrectly() {
+		byte[] bytearray=new byte[]	{0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+		byte[] expected=new byte[]	{0x01,0x02,(byte)0xfc,(byte)0xfd,(byte)0xfe,(byte)0xff,0x07,0x08};
+		int longword=0xfffefdfc;
+		int offsetToWriteLongword=2;
+		ByteTools.lWriteLongwordToBytearray(bytearray,offsetToWriteLongword,longword);
+		assertArrayEquals(expected,bytearray);
 	}
 
 	@Test
@@ -121,13 +281,13 @@ class ByteToolsTest {
 		byte[] expected=new byte[] {0x01,0x02,0x03,0x04};
 		assertArrayEquals(expected,ByteTools.bHexStringToByteArray("01020304"));
 	}
-	
+
 	@Test
 	void negativeHexStringIsConvertedToBigEndianBytearrayCorrectly() {
 		byte[] expected=new byte[] {(byte)0xff,(byte)0xfe,(byte)0xfd,(byte)0xfc};
 		assertArrayEquals(expected,ByteTools.bHexStringToByteArray("fffefdfc"));
 	}
-	
+
 	@Test
 	void compareBytesReturnsTrueCorrectly() {
 		byte[] bytearray1=new byte[] {0x01,0x02,0x03,0x04};
@@ -135,7 +295,7 @@ class ByteToolsTest {
 		int offset=2;
 		assertTrue(ByteTools.compareBytes(bytearray1, bytearray2, offset));
 	}
-	
+
 	@Test
 	void compareBytesReturnsFalseCorrectly() {
 		byte[] bytearray1=new byte[] {0x01,0x02,0x03,0x04};
@@ -143,5 +303,5 @@ class ByteToolsTest {
 		int offset=2;
 		assertFalse(ByteTools.compareBytes(bytearray1, bytearray2, offset));
 	}
-	
+
 }
